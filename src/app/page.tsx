14af8d01ff49cfa0ffaae9bd8dd4daefcecdc03f
Patryk Baraniak <pb311239@student.polsl.pl>
2024-09-30 15:21:21 +0200

    jw

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-import { Button } from "@/components/ui/button";
 import { BookOpen, Trophy } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
