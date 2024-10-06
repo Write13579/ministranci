de6691e126b0725c2431d4845689ed4109a3efb6
PiotrekPKP <peterpkppeter@gmail.com>
2024-10-06 21:36:08 +0200

    feat: added cell badge

--- src/app/listaMinistrantow/columns.tsx
+++ src/app/listaMinistrantow/columns.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { Badge } from "@/components/ui/badge";
 import { UserRanga } from "@/lib/database/scheme";
 import { ColumnDef } from "@tanstack/react-table";
+import clsx from "clsx";
 
 export type Ministrant = {
   id: number;
@@ -20,7 +22,29 @@ export const columns: ColumnDef<Ministrant>[] = [
     accessorKey: "pseudonim",
     header: "Pseudonim",
   },
-  { accessorKey: "ranga", header: "Ranga" },
+  {
+    accessorKey: "ranga",
+    header: "Ranga",
+    cell: ({ row }) => {
+      const value = row.getValue("ranga") as UserRanga;
+
+      return (
+        <Badge
+          className={clsx({
+            "bg-blue-500 hover:bg-blue-400": value === UserRanga.ALBA,
+            "bg-green-500 hover:bg-green-400": value === UserRanga.ANIMATOR,
+            "bg-yellow-500 hover:bg-yellow-400":
+              value === UserRanga.BEZKOLNIERZ,
+            "bg-black hover:bg-gray-700": value === UserRanga.CZARNY,
+            "bg-purple-500 hover:bg-purple-400": value === UserRanga.KANDYDAT,
+            "bg-indigo-500 hover:bg-indigo-400": value === UserRanga.KOLNIERZ,
+          })}
+        >
+          {value}
+        </Badge>
+      );
+    },
+  },
   { accessorKey: "wiek", header: "Wiek" },
   { accessorKey: "miesiacPrzystapienia", header: "Miesiąc przystąpienia" },
   { accessorKey: "czasSluzby", header: "Czas służby (w miesiącach)" },
