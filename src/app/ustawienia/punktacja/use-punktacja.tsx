c8de773ff7a0d1bb538af97a6a61aa8cf1c519f8
PiotrekPKP <peterpkppeter@gmail.com>
2024-10-09 15:50:25 +0200

    unused

--- src/app/ustawienia/punktacja/use-punktacja.tsx
+++ src/app/ustawienia/punktacja/use-punktacja.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { PunktacjaData } from "./page";
 import { savePunktacja } from "./actions";
 import { useRouter } from "next/navigation";
